5c2671ab84ee5aa24d730c5f8b04e36db5e63a96
raymysco <134584327+raymysco@users.noreply.github.com>
2025-12-16 18:21:59 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "raymy-cache-v1";
+const urlsToCache = [
+  "/index.html",
+  "/admin.html",
+  "/blog.html",
+  "/css/style.css",
+  "/js/script.js"
+];
+
+// Install event
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Activate event
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => 
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) return caches.delete(cache);
+        })
+      )
+    )
+  );
+});
+
+// Fetch event
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
