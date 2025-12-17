dc26fe3181ef5e6ec0233b499412ffd28a1ac750
raymysco <134584327+raymysco@users.noreply.github.com>
2025-12-17 19:02:51 +0100

    Refactor script.js for better organization

--- js/script.js
+++ js/script.js
@@ -8,13 +8,6 @@ menuBtn.addEventListener("click", () => {
   navLinks.classList.toggle("menu-open");
 });
 
-/* Close menu when a link is clicked */
-document.querySelectorAll(".nav-links a").forEach(link => {
-  link.addEventListener("click", () => {
-    navLinks.classList.remove("menu-open");
-  });
-});
-
 
 /* =============================
    SCROLL ANIMATION (SERVICES)
